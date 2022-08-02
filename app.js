e943106e5c4b0546c7dacbcd766a826cacd3b4d8
Saran barua <Saranbarua2357@gmail.com>
2022-08-02 13:17:51 +0600

    router updated

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ app.use(express.json())
 
 require('dotenv').config()
 const userRoute = require("./routes/userRoutes");
+const workExp=require("./routes/workExp");
 mongoose.connect(process.env.DB)
 
 app.use("/user", userRoute)
-
+app.use("/user",workExp )
 app.listen(process.env.PORT, ()=>{
     console.log("Listening..")
 })                                                                                  
